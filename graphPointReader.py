aff25230c99783d1c833edc7d48f47cfd3841fde
Joe Prollins <joe.prollins@hotmail.com>
2022-09-02 01:09:43 +0100

    added more comments and example graph

--- graphPointReader.py
+++ graphPointReader.py
@@ -1,31 +1,27 @@
 print("Loading modules")
 from os import environ
-environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
-import openpyxl 
+import openpyxl
 from easygui import *
 from time import sleep
-
-
-
-
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 def exInputOpenSave(rowInput, columnInput, cellInput, fileName):
+    '''
+    Opens a given excel file, enters data into a given cell given the row and column and then saves
+    '''
     excel = openpyxl.load_workbook(fileName)
     excelSht = excel.active
     excelSht.cell (row = rowInput, column = columnInput).value = cellInput
     excel.save(fileName)
-    pass
-
-#exInputOpenSave(1,1,"reee", sourceFile)
 
 def graph():
     '''
     Displays the graph on the window. This also essentially cleans the graph from previous markings
     '''
     screen.blit(pygame.transform.scale(graphImg, (size)),(graphX,graphY))
-    
-    
+
+
 def drawTarget(x,y,colour):
     '''
     Draws a crosshair on the screen
@@ -33,7 +29,7 @@ def drawTarget(x,y,colour):
     pygame.draw.rect(screen, colour, [x,y-1, 3000, 2])
     pygame.draw.rect(screen, colour, [x-1,y, 2, 3000])
     pygame.draw.rect(screen, colour, [ -2, -2, x+1,y+1], 2)
-    
+
 def message_display(text):
     '''
     Creates & displays the message saying the x and y values
@@ -60,23 +56,17 @@ def messageUpdate(xPos, yPos, roundX, roundY):
 # Name that will be on all the captions for the selection boxes
 programName = "Joe Prollins' Graphical Reader"
 
-
 msg = "Enter the name that you want your excel to be named. Don't include the file extension. \nNote: this will overwrite a file with the same name."
 title = programName
 sourceFile = []
 sourceFile = enterbox(msg, title, sourceFile)
 sourceFile = sourceFile + ".xlsx"
 
-
-
 excel = openpyxl.Workbook()
-#sourceFile = "test.xlsx"
 excel.save(sourceFile)
-
 
 # Intialises the pygame module
 pygame.init()
-
 
 msgbox("Select the graph you wish to upload.")
 
@@ -115,43 +105,44 @@ pygame.display.set_caption(programName)
 
 # Loads in the graphical image
 graphImg = pygame.image.load(imageFile)
-# Settomg the coordinates for where the graph will be placed
+# Setting the coordinates for where the graph will be placed
 graphX = 0
 graphY = 0
-# Setting up the colours 
+# Setting up the colours
 red = (255,0,0)
 blue = (0,0,255)
 green = (0, 255, 0)
 
-
-
- 
-
+# Calibraqtion setting set to true, so when app starts, it will start calibrating
 calibration = True
 calibrateAxis = False
 allowEvent = True
+# Row in the Excel sheet where data will start being entered
 dataEntryRow = 2
 exInputOpenSave(1,1,"x axis", sourceFile)
 exInputOpenSave(1,2,"y axis", sourceFile)
 
-
-
-
 running = True
+# Draws the background image
 graph()
+#  App starts
 while running:
+    # When player quits (i.e presses the top right x), the application stops
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit()
             break
             running = False
 
+    # Calibrating the origin point
     if calibration == True:
+        # When user left clicks, draws a target on a fresh background
         if event.type == pygame.MOUSEBUTTONDOWN:
             graph()
             pos = pygame.mouse.get_pos()
             drawTarget(pos[0],pos[1],red)
             allowEvent = True
+        # If the target exists, and the user presses enter, sets the origin point and changes the target colour
         if event.type == pygame.KEYDOWN and allowEvent == True:
             if event.key == pygame.K_RETURN:
                 originX = pos[0]
@@ -162,13 +153,16 @@ while running:
                 calibration = False
                 allowEvent = False
 
-
+    # Calibrating the axes
     if calibrateAxis == True:
+        # When user left clicks, draws a target on a fresh background
         if event.type == pygame.MOUSEBUTTONDOWN:
             graph()
             pos = pygame.mouse.get_pos()
             drawTarget(pos[0],pos[1],red)
             allowEvent = True
+        # If the target exists, and the user presses enter, gets the pixel coordinates of the target, then asks the user what the graphical coordinates and the 
+        # percision they would like to recieve values at. Then sets the axis scales.
         if event.type == pygame.KEYDOWN and allowEvent == True:
             if event.key == pygame.K_RETURN:
                 selectionX = pos[0]
@@ -178,13 +172,15 @@ while running:
                 pygame.display.update()
                 print(pos[0],pos[1])
                 calibrateAxis = False
-                msg = "What is the value of the point you have selected?\nHow many decimal places do you want the results to round to?\nAfter submiting the values, clicking anywhere will yield the values, pressing the 'enter' key will log the values in an excel file." 
+                msg = "What is the value of the point you have selected?\nHow many decimal places do you want the results to round to?\nAfter submiting the values, clicking anywhere will yield the values, pressing the 'enter' key will log the values in an excel file."
                 title = programName
                 fieldNames = ["x value", "y value", "x rounding", "y rounding"]
                 fieldValues =[]
                 fieldValues = multenterbox(msg,title, fieldNames)
+                # Coordinate points
                 trueX = float(fieldValues[0])
                 trueY = float(fieldValues[1])
+                # Scale Factor for each axis
                 scaleX = (selectionX - originX) / trueX
                 scaleY = (selectionY - originY) / trueY
                 roundX = int(fieldValues[2])
@@ -193,8 +189,9 @@ while running:
                 sleep(.2)
 
 
-    
+    # Calibration is finished, so everytme the user clicks on the graph, they get the graphical coordinates they then can record the values
     if calibration == False and calibrateAxis == False:
+        # User selects a point, the graphical coordinates get printed to terminal and in GUI
         if event.type == pygame.MOUSEBUTTONDOWN:
             graph()
             pos = pygame.mouse.get_pos()
@@ -206,7 +203,7 @@ while running:
             messageUpdate(xPos, yPos, roundX, roundY)
             sleep(.2)
             allowEvent = True
-            pass
+        # If the user presses enter, the coordinates will be saved in an excel file
         if event.type == pygame.KEYDOWN and allowEvent == True:
             if event.key == pygame.K_RETURN:
                 graph()
@@ -224,16 +221,6 @@ while running:
             drawTarget(pos[0], pos[1], blue)
         except:
             pass
-            
+
 
     pygame.display.update()
-
-
-
-
-
-
-
-
-
-
